98e8bc8df574851c74f6ba9869d0f9993fa9d89c
Aaron545 <avalladaresazuaje@byupathway.edu>
2025-04-19 07:15:05 -0400

    Memory-card-game all basic functions completed

--- memory-card-game.js
+++ memory-card-game.js
@@ -18,11 +18,36 @@ function randomize_cards_image() {
   return cards_number
 }
 
+// This function checks if two cards are not pair, returns "true" if
+// they don't match, and "false" if they match.
+function check_if_cards_dont_match(index, index_of_card_array, cards) {
+  index_of_card_array.push(index)
+  
+  console.log(index_of_card_array.length)
+  if (index_of_card_array.length == 2) {
+    if (cards[index_of_card_array[0]].children[0].src == cards[index_of_card_array[1]].children[0].src) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+}
+
+// This function flips back the cards if they are not the same
+function flip_back_if_not_match(index_of_card_array, cards) {
+  console.log(index_of_card_array)
+  cards[index_of_card_array[0]].children[0].src = "images/pngwing.com(8).png"
+  cards[index_of_card_array[1]].children[0].src = "images/pngwing.com(8).png"
+  index_of_card_array.length = 0;
+}
 
 // Here ("main" function) will be most of the code
 function main() {
   // make an array to contain the cards elements (divs)
   let cards = [];
+  let index_of_card_array = [];
+  result = "";
 
   // get the card elements from html, and store them inside the array variable that i created
   // so i am able to use it
@@ -39,6 +64,17 @@ function main() {
   // whatever code that is inside the eventListener brackets "{}".
   for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", () => {
+
+      // if the result of the "check_if_cards_dont_match" function is equal to == "true"
+      // (the cards don't match), the "flip_back_if_not_match" function is gonna flip or
+      // return them to their original image (the question mark), if they do match
+      // they are gonna stay flipped (the image can be seen).
+      if (result == true) {
+        flip_back_if_not_match(index_of_card_array, cards)
+      } else if (result == false) {
+        index_of_card_array.length = 0;
+      }
+
       /* When you click a card, the switch statement detects what card you clicked and uses the 
       "card_number_random" array, that contains numbers from 1-11 randomly orderer, this numbers will be used
       in each "case" inside the switch to randomly choose in what card each image will be. It does this by changing
@@ -68,7 +104,10 @@ function main() {
         case cards_number_random[10]:
         case cards_number_random[11]:
           cards[i].children[0].src = "images/pngwing.com(6).png";
-        }
+      }
+
+      result = check_if_cards_dont_match(i, index_of_card_array, cards)
+      console.log(result)
 
     })
   }
